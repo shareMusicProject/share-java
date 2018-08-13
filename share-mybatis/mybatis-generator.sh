fb5545aa633c732685123963d5d6ab057f0bcbe7
YZSPSJK <769478813@qq.com>
2018-08-13 16:53:04 +0800

    使用mybatis generater插件

--- share-mybatis/mybatis-generator.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-mvn mybatis-generator:generate -Ddb=cupid -e
---
+++ share-mybatis/src/main/java/com/backend/share/mybatis/mapper/ext/UserMapperExt.java
@@ -0,0 +1,7 @@
+package com.backend.share.mybatis.mapper.ext;
+
+import com.backend.share.mybatis.mapper.UserMapper;
+
+public interface UserMapperExt extends UserMapper{
+
+}
