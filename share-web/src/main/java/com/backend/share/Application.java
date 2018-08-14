d08c40edc7c3780de8abd45b0ecc6f0d3cac30b0
YZSPSJK <769478813@qq.com>
2018-08-14 15:38:20 +0800

    增加swagger2

--- share-web/src/main/java/com/backend/share/Application.java
+++ share-web/src/main/java/com/backend/share/Application.java
@@ -1,16 +1,11 @@
-package com.backend.share.web;
+package com.backend.share;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.core.env.Environment;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Pandora Boot应用的入口类
@@ -19,12 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author chengxu
  */
-//@MapperScan("com.aliyun.sop.cupid.mybatis.mapper")
-//@SpringBootApplication(scanBasePackages = {"com.backend.share"})
-//@ImportResource({ "classpath:applicationContext.xml" })
-//@EnableTransactionManagement
-//@ServletComponentScan
 @RestController
+@EnableSwagger2
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Application {
 
---
+++ share-web/src/main/java/com/backend/share/web/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.backend.share.web.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@Api(value = "PageController", description = "用户登录登出接口")
+@RequestMapping("/test")
+public class TestController {
+    @ApiOperation(value="用户登录", notes="用户登录接口")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "username", value = "用户名", required = true ,dataType = "string"),
+            @ApiImplicitParam(name = "password", value = "密码", required = true ,dataType = "string")
+    })
+    @RequestMapping(value = "/login",method = {RequestMethod.POST,RequestMethod.GET})
+    @ResponseBody
+    public String login(HttpServletRequest request){
+        return "success";
+    }
+
+}
